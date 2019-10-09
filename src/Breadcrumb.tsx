92a88cfa92b003dd6baf8e4211347af8a8dfc6ce
Shelly <pxshelly@gmail.com>
2019-10-08 22:39:11 -0700

    Render each value into Breadcrumbs component

--- src/Breadcrumb.tsx
+++ src/Breadcrumb.tsx
@@ -5,7 +5,21 @@ export interface Props {
 }
 
 export class Breadcrumb extends React.PureComponent<Props> {
+  getLiListFromValues(values: number[]): Element[] {
+    return values.map((value: number, index: number) => <li key={index}>{this.formatValue(value)}</li>);
+  }
+  
+  formatValue(value: number) {
+    return value.toFixed(3);
+  }
+
   render() {
-    return <div />;
+    return (
+      <div className="breadcrumb">
+        <ul>
+          {this.getLiListFromValues(this.props.values)}
+        </ul>
+      </div>
+    );
   }
 }
